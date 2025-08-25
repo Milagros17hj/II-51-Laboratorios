a2c4fec548d255eef114d4a09731cbd51f785825
Milagros Hernández <mhernandezj@edu.uc.ac.cr>
2025-08-25 11:16:19 -0600

    Agregar configuración de API y estilos, incluyendo la inclusión de archivos de configuración y la creación de estilos CSS para la interfaz de usuario.

---
+++ Lab08/api/config_example.php
@@ -0,0 +1,4 @@
+<?php
+$apikey = 'TU_API_KEY';
+$urlBase = 'API_URL_BASE';
+?>
--- Lab08/api/usuarios.php
+++ Lab08/api/usuarios.php
@@ -1,11 +1,11 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-  header("Access-Control-Allow-Headers: Content-Type, Authorization");
-  header("Access-Control-Allow-Credentials: true");
+  //header("Access-Control-Allow-Origin: *");
+  //header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+  //header("Access-Control-Allow-Headers: Content-Type, Authorization");
+  //header("Access-Control-Allow-Credentials: true");
 
-$apikey = 'sb_publishable_YCeMU_xbGAhPt7qB5roXSQ_KNuBXFuw';
-$urlBase = 'API_KEY'
+include_once 'config.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 function callSupabase($method, $url, $data = null) {
